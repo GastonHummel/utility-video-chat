556cf17709efcd462a49b149232ffcbc1dacff66
Gaston Hummel <gaston.hummel@precisely.com>
2021-04-23 13:37:02 +0200

    first commit

---
+++ js/test.js
@@ -0,0 +1,19 @@
+//   $.post(
+//     'https://xsell.pro/onDemandWebinar',
+//     {
+//       firstName,
+//       lastName,
+//       email,
+//       company,
+//       repEmail,
+//       repFirstName,
+//       repName,
+//       campaignId,
+//       acctRegion,
+//       webinar,
+//       action,
+//     },
+//     function (data) {
+//       console.log(JSON.stringify(data, null, 2));
+//     },
+//   );
